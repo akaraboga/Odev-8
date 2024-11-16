6a926d136f7ae8a8a99f2a465c7562fa96d9e7ee
Ahmet Karaboğa <130494000+akaraboga@users.noreply.github.com>
2024-11-16 23:06:12 +0300

    Add files via upload

---
+++ odev-8/odev-8-degistirme.sql
@@ -0,0 +1,32 @@
+UPDATE test --Odev-8 Sütun değer değiştirme
+--SET
+	--FIRST_NAME = 'Ahmet',
+	--LAST_NAME = 'Karaboga',
+	--EMAIL = 'a.karabogaa',
+	--BIRTHDAY = '2000.06.29'
+--WHERE ID = 1; --ID si 1 olan sütunun değerini değiştirme
+
+--SET 
+	--first_name='Etebo' 
+	--Where first_name='Helaine';--Adı Helaine olan Sütünün adını değiştirme
+
+--SET 
+	--last_name='Karo'
+	 --Where first_name='Etebo'; -- İlk adı etebo olan sütünun soyadını değiştirme
+
+--SET 
+	-- email='blabla.com'
+	 
+--	Where  ID=20; --Id si 20 olan sütünün 2 mailini değiştirme
+
+Set 
+
+	email='.com'
+	Where email LIKE '%u';--Sonu u ile biten emailleri değiştirme
+
+
+ 
+select * from test
+ 
+
+ 
---
+++ odev-8/odev-8-silme.sql
@@ -0,0 +1,8 @@
+Delete From test
+
+--Where id='20';
+--Where id ='21';
+--Where first_name='Raddie';
+
+
+Select * From test
